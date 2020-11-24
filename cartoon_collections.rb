ef3dd4e0c2fb4c3866ad4f9c5d8e8ce233b2484f
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-24 19:22:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |string, index|
-    puts array[string] = index
+    puts array(string) = index
   end
 end
